40abcd24ff886f039091c1134b63e22c32fb48e9
fin <fin@fin-laptop.(none)>
2011-05-16 07:39:18 +0400

    Added solution for getting multiple integers. Now return array with random integers.

--- lib/random_org.rb
+++ lib/random_org.rb
@@ -6,22 +6,23 @@ module RandomOrg
 	class Integer
 		@@url = URI.parse("http://random.org/integers/")
 		ValidBases = [2, 8, 10, 16]
+    ValidNums = [1...1000]
 		
-		attr_reader :min, :max, :base, :value, :opts
+		attr_reader :min, :max, :base, :value, :opts, :num
 		
-		def initialize(min, max, base=10)
+		def initialize(min, max, base=10, num=1)
+      @num = num.to_s
 			@min = min.to_s
 			@max = max.to_s
 			@base = base.to_s
-			@@url.query = "num=1&min=#{@min}&max=#{@max}&col=1&base=#{@base}&format=plain&rnd=new"
+			@@url.query = "num=#{@num}&min=#{@min}&max=#{@max}&col=#{@num}&base=#{@base}&format=plain&rnd=new"
 			
 			begin
 				res = Net::HTTP.get(@@url).chomp
-				if @base.to_i == 16
-					@value = res
-				else
-					@value = res.to_i
-				end
+				if @base.to_i != 16
+          res.each { |x| x.to_i }
+        end
+        @value = res
 			rescue Net::HTTPFatalError => e
 				puts "Error: " + e
 			end
